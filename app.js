c1bd709207f98d5cfd0fcb94d9f36b67804700e2
franciscocaballero <caballerofrancisco22@yahoo.com>
2022-01-06 21:50:17 -0500

    commitg solution

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-console.log("hello world");
+// Coding Example 1:
+// let string = "";
+
+// for (let i = 0; i <= 7; i++) {
+//   string += "#";
+//   console.log(string);
+// }
+
+// Fizz Buzz:
+// for (let i = 1; i <= 100; i++) {
+//   if (i % 3 === 0 && i % 5 === 0) console.log("fizzbuzz");
+//   else if (i % 3 === 0) console.log("fizz");
+//   else if (i % 5 === 0) console.log("buzz");
+
+//   console.log(i);
+// }
+
+// ChessBoard:
+let ChessBoard = function (num) {
+  for (let i = 1; i <= num; i++) {
+    if (i % 2 === 0) console.log(" # # # #");
+    else if (i % 2 !== 0) console.log("# # # #");
+  }
+};
+
+ChessBoard(16);
